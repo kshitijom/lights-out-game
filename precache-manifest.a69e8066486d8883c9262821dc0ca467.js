4ee3b446a414aaf312d7a0a5385a5dd3b24b29c9
kshitijom <mishrakshitij.646@gmail.com>
2020-08-21 20:44:46 +0530

    Updates

---
+++ precache-manifest.a69e8066486d8883c9262821dc0ca467.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03598ed302eda289f4afc7be7b6697bf",
+    "url": "/lights-out-game/index.html"
+  },
+  {
+    "revision": "991c5d9699cca58e5dda",
+    "url": "/lights-out-game/static/css/main.3ee11aaa.chunk.css"
+  },
+  {
+    "revision": "75c247a0b9d42c79dfb5",
+    "url": "/lights-out-game/static/js/2.0a61412d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lights-out-game/static/js/2.0a61412d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "991c5d9699cca58e5dda",
+    "url": "/lights-out-game/static/js/main.4122b557.chunk.js"
+  },
+  {
+    "revision": "ba00fdeb71ddb07ece2d",
+    "url": "/lights-out-game/static/js/runtime-main.abfbb1c9.js"
+  }
+]);
